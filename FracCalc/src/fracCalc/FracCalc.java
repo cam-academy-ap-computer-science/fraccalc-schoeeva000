a28112492b9e5d13ef3afa62c4799a0e725dffc6
schoeeva000 <schoeeva000@CAM-LAB-26.bgsd.k12.wa.us>
2019-12-09 07:55:26 -0800

    fasdfasdf

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -31,61 +31,78 @@
  * 
  */
 
-
 package fracCalc;
+
 import java.util.Scanner;
 
 public class FracCalc {
 
-    public static void main(String[] args) 
-    {
-        // TODO: Read the input from the user and call produceAnswer with an equation
-    	Scanner userJunk = new Scanner(System.in);
-    	String initialInput = "";
-    	while (!initialInput.toLowerCase().equals("quit")) {  //if the user didn't want to quit, go on with the calculator
-    		System.out.println("Enter equation to calculate, or type \"quit\" to quit.");
-        	System.out.println("Seperate fractions with \"/\", whole numbers from fractions with \"_\", and numbers from operators with a space.");
-        	initialInput = userJunk.nextLine();
-    		System.out.println(produceAnswer(initialInput)); // print the answer!
-    	}
-    	
-    	System.out.println("Goodbye!");
-    	
-    }
-    
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //        
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    { 
+	public static void main(String[] args) {
+		// TODO: Read the input from the user and call produceAnswer with an equation
+		Scanner userJunk = new Scanner(System.in);
+		String initialInput = "";
+		while (!initialInput.toLowerCase().equals("quit")) { // if the user didn't want to quit, go on with the
+																// calculator
+			System.out.println("Enter equation to calculate, or type \"quit\" to quit.");
+			System.out.println(
+					"Seperate fractions with \"/\", whole numbers from fractions with \"_\", and numbers from operators with a space.");
+			initialInput = userJunk.nextLine();
+			System.out.println(produceAnswer(initialInput)); // print the answer!
+		}
+
+		System.out.println("Goodbye!");
+
+	}
+
+	// ** IMPORTANT ** DO NOT DELETE THIS FUNCTION. This function will be used to
+	// test your code
+	// This function takes a String 'input' and produces the result
+	//
+	// input is a fraction string that needs to be evaluated. For your program, this
+	// will be the user input.
+	// e.g. input ==> "1/2 + 3/4"
+	//
+	// The function should return the result of the fraction after it has been
+	// calculated
+	// e.g. return ==> "1_1/4"
+	public static String produceAnswer(String input) { 
         // TODO: Implement this function to produce the solution to the input
+    	
     	int numberOfValues = 1;
     	String cutInput = input;
     	while (cutInput.indexOf(' ') != -1) { //finds the length of the whole string by searching for operators
     		cutInput = cutInput.substring(cutInput.indexOf(' ') + 3, cutInput.length() - 1); //cut out a smaller string to search next time
     		numberOfValues++;
-    		System.out.println(numberOfValues);
     	}
     	
-        
-        return "";
-        
-     /*   
+    	int wholeNumbers[] = new int[numberOfValues];
+    	int numerators[] = new int[numberOfValues];
+    	int denominators[] = new int[numberOfValues];
+    	
+    	cutInput = input;
+    	while (cutInput.length() > 0) {
+    		String whole = cutInput.substring(0, cutInput.indexOf('_'));
+    		String num = cutInput.substring(cutInput.indexOf('_'), cutInput.indexOf('/'));
+    		String denom = cutInput.substring(cutInput.indexOf('/'), cutInput.indexOf('/') + 2);
+    		cutInput = "";
+    		System.out.println(cutInput + " " + num);
+    		System.out.println(denom);
+    		System.out.println(whole);
+    	}
+    	return "";
+    	
+
+      /*
      public static int gcd(int a, int b) {
        if (b == 0) {
         	return a;
        	}
         return Math.abs(gcd(b, a%b));
-     } */
+     } 
 
     
  // TODO: Fill in the space below with any helper methods that you think you will need
-    /*
+    
     public static int gcd(int a, int b) {
     	if (b == 0) {
     		return a;
@@ -93,7 +110,5 @@ public class FracCalc {
     	return Math.abs(gcd(b, a%b));
     }
     */
-}}
-
-    
-
+}
+}
